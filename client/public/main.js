42af6cf1dd71cabd8cec95e804676f610036637d
deepak <deepak.singh@qversity.com>
2019-07-30 17:39:55 +0530

    add files

--- client/public/main.js
+++ client/public/main.js
@@ -34,7 +34,7 @@ if(!AgoraRTC.checkSystemRequirements()) {
     
     var channel_key = null;
     var appId = '748f9639fa864651bef8419d5870ec50';// provided by arjun 
-    var appId = '232f270a5aeb4e0097d8b5ceb8c24ab3';
+    // var appId = '232f270a5aeb4e0097d8b5ceb8c24ab3';
     
     console.log("Init AgoraRTC client with App ID: " + appId);
     

bf0ac5b95c6543c34fbcd2b4ece2df6bd6c86f7f
deepak <deepak.singh@qversity.com>
2019-07-30 16:41:57 +0530

    add files

--- client/src/App.js
+++ client/src/App.js
@@ -48,7 +48,7 @@ class App extends Component {
         <Router>
       <div className="App">
         <Navbar />
-        <Route exact path="/" component={Landing} />
+        <Route exact path="/" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
           <Switch>

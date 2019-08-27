b9213ac3d34b76cee98c5881dc3307b55b4c8857
deepak <deepak.singh@qversity.com>
2019-08-27 15:57:38 +0530

    local changes

--- api/v1/controllers/user/UserCtrl.js
+++ api/v1/controllers/user/UserCtrl.js
@@ -5,7 +5,9 @@ const underscore = require("underscore");
 const userModel = require('../../models/User');
 const tokenModel = require('../../models/AuthToken');
 const sessionInfoModel = require('../../models/SessionInfo');
+const sessionModel = require('../../models/Session');
 const clientToken = require( process.cwd() + '/util/ClientToken');
+const utils = require(process.cwd() + '/util/Utils');
 
 const saltRounds = 10;
 
@@ -36,6 +38,7 @@ class UserCtrl {
 	    	let email = req.body.email;
 	    	let password = req.body.password;
 	    	let userObj = await userModel.getUserByEmail(email);
+	    	console.log('----------------------%%%%%%%%%%%', userObj)
 	    	
 			if(!isEmpty(userObj)){
 				// let hashedPassword = await bcrypt.hash(password, saltRounds);
@@ -43,9 +46,27 @@ class UserCtrl {
         		let t = await bcrypt.compare(password, userObj.password);
 				if(t){
 					const token = await auth.createToken(userObj.id);
-					console.log('token-------------',token);
-					let updateUser = tokenModel.updateToken(userObj.id, token);
-					
+					// console.log('token-------------',token);
+					let updateUser = await tokenModel.updateToken(userObj.id, token);
+
+					userObj = underscore.extend(userObj, {token:token});
+
+					let currentSession = await sessionModel.getUpcommingSession(userObj.id);
+
+					let sessionData = {};
+					if(!isEmpty(currentSession)){
+						currentSession = currentSession[0];
+						console.log(currentSession, currentSession.scheduleDate);
+						let SessionDt = currentSession.scheduleDate;
+						utils.dateTimeDiff(currentSession.scheduleDate);
+
+					} else {
+						sessionData = {
+							sessionData : { message : "There are no sessions available."}
+						}
+					}
+						underscore.extends(userObj, sessionData);
+
 					res.status(200).send({token:token, id:userObj.id, name:userObj.name, email:userObj.email, userType:req.body.type});
 				} else {
 					res.status(400).send({password:"Invalid password"})
--- api/v1/models/Session.js
+++ api/v1/models/Session.js
@@ -5,6 +5,26 @@ class Session{
 	constructor(){
 		this.table = 'sessions';
 	}
+
+	async getUpcommingSession(userId) {
+		try
+	    {
+	        return await new Promise((resolve, reject) => {
+            	db.query('SELECT s.*, su.* FROM sessions s LEFT JOIN session_users su ON su.sessionId = s.id WHERE su.userId = ? AND su.status = 1 AND s.status = 1 AND s.scheduleDate IS NOT NULL AND s.scheduleDate > ( NOW() - INTERVAL 10 MINUTE ) ORDER BY s.scheduleDate ASC LIMIT 1', [userId], function (error, results, fields) {
+				  if (error) reject(error);
+				  // console.log('================== results ', results)
+				  // db.end();
+				  return resolve(results);
+				});
+	        })
+	    }
+	    catch(err)
+	    {
+	    	console.log(err)
+	       return err;
+	    }
+	}
+
 }
 
 module.exports = new Session();

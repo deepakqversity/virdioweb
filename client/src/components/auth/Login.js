fdb48133830cee74fafe6446caa4bb1f276e238f
deepak <deepak.singh@qversity.com>
2019-09-12 11:48:36 +0530

    add remember me and is banned

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -6,6 +6,8 @@ import { connect } from "react-redux";
 import { loginUser } from "../../actions/authActions";
 import classnames from "classnames";
 
+import $ from 'jquery';
+
 class Login extends Component {
   constructor() {
     super();
@@ -29,6 +31,14 @@ class Login extends Component {
         this.props.history.push("/guest");
       } else {
         this.props.history.push("/home");
+      }
+    } else {
+      if (localStorage.chkbx && localStorage.chkbx != '') {
+          $('#remember_me').attr('checked', 'checked');
+          $('#email').val(localStorage.email);
+      } else {
+          $('#remember_me').removeAttr('checked');
+          $('#email').val('');
       }
     }
   }
@@ -95,7 +105,7 @@ return (
                 <div className="login-inner">
                 <div className = "form-group mb-4 mt-4">
                     <span className="text-danger">{errors.email}{errors.emailincorrect}{errors.message}</span>
-                    <label>Enter your email id</label>
+                    <label>Enter your email address</label>
                     <input type="email"  id="email" onChange={this.onChange} value={this.state.email}  error={errors.email}  className={classnames("", { invalid: errors.email || errors.emailincorrect })} className = "form-control"  />
                   <img src="/images/login-user.png" className="user-login" />
                 </div>
@@ -107,22 +117,13 @@ return (
                     <img src="/images/login-user.png" className="user-login" />
                 </div>
                 
-                {/*<div className = "form-group">
-                  <div class = "form-check-inline radio">
-                      <label>
-                          <input type = "radio" name = "type" id = "host" onChange={this.handleChange} value = '1' checked /> Host
-                      </label>
-                    </div>
-                    <div class="form-check-inline radio">
-                      <label>
-                          <input type = "radio" name = "type" id = "client" onChange={this.handleChange} value = '2'  checked={this.state.type === "2"}  /> Client
-                      </label>
-                    </div>
-                  </div>*/}
-      
                 <div className = "form-group pt-3 mb-4">
                     <div className = "d-flex flex-wrap justify-content-between align-items-center">
-                      <button type = "submit" className="btn-cancel btn btn-large btn-outline-secondary waves-effect waves-light hoverable blue accent-3 rounded p-3 px-4">Cancel</button>
+                    <div className="remember_me">
+                        <input type="checkbox" tabIndex="3" value="remember-me" id="remember_me" />Remember Me
+                    </div>
+
+                      <button type = "button" className="btn-cancel btn btn-large btn-outline-secondary waves-effect waves-light hoverable blue accent-3 rounded p-3 px-4">Cancel</button>
                       <button type = "submit" className="btn-login btn btn-large btn-primary waves-effect waves-light hoverable blue accent-3 p-3 px-4 rounded">Log in</button>
                       <a href="#" className="forgot-password mt-sm-0 mt-3">Forgot password?</a>
                     </div>

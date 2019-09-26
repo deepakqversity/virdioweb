bc1b5d54e8bf8b65d26231e21066551ff59713f3
deepak <deepak.singh@qversity.com>
2019-09-26 17:28:28 +0530

    add files

--- client/src/components/home/Host.js
+++ client/src/components/home/Host.js
@@ -202,7 +202,7 @@ render() {
           <tr data-position="100000000000000" id={"online-user-row-"+id} key={idx}>
           <th scope="row"><img src={image} /></th>
           <td className="text-left"><span className="welcome-title">{firstName.toLowerCase()} {lastName != null ? lastName.toLowerCase() : ''} {city != null ? ', '+city.toLowerCase() : ''}</span></td>
-          <td><img className="mr-2 user-status" src="/images/offline.png" /><span className="user-online-status">online</span></td>
+          <td><img className="mr-2 user-status" src="/images/offline.png" /><span className="user-online-status">offline</span></td>
           <td className="visible-status"><i className="fa fa-check text-green"></i><i className="fa fa-times text-red d-none"></i></td>
           <td>5</td>
           </tr>

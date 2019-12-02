ffb2a8e2c915ae9eaaee06e34b7f9330405d84b7
Jagat Prakash <jagat@qversity.com>
2019-12-02 23:22:37 +0530

    swapping

--- client/public/main.js
+++ client/public/main.js
@@ -2740,7 +2740,7 @@ function signalHandler(uid, signalData, userType) {
         }
     }
 
-    /*function switchAudienceToBroadcaster(){
+    function switchAudienceToBroadcaster(){
         let audience = getAllAudience();
         if(audience != null) {
             for(let i in audience){
@@ -2760,9 +2760,9 @@ function signalHandler(uid, signalData, userType) {
               }
             }
         }
-    }*/
+    }
 
-    function switchAudienceToBroadcaster(){
+    /*function switchAudienceToBroadcaster(){
         let audience = getAllAudience();
         if(audience != null) {
 
@@ -2801,7 +2801,7 @@ function signalHandler(uid, signalData, userType) {
               }
             }
         }
-    }
+    }*/
 
     function pushIntoSessionByHost(){
       let uid = '';
